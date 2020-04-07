2a57cf07fcf035928862f41b68a3d65130c3f3df
Vishal <geek4teck@gmail.com>
2020-04-07 16:45:44 +0530

    Bug Fixes

--- index.js
+++ index.js
@@ -46,6 +46,4 @@ const randomNumber = (min, max) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-module.exports = {
-    generateTickets: generateTickets
-  }
+export const generateTickets = generateTickets;
