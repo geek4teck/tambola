7b2b1cb0a5527952098280c5802455f28009f2a0
Vishal <geek4teck@gmail.com>
2020-04-07 16:51:45 +0530

    Bug Fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const generateTickets = (tickets) => {
-    let cols, finalTicket, finalTickets = [], colPlaceholder = [];
+function generateTickets(tickets) {
+    var cols, finalTicket, finalTickets = [], colPlaceholder = [];
     for (b = 0; b < tickets; b++) {
         cols = Array(9).fill(2);
         finalTicket = Array(3);
         finalTicket[0] = Array(9).fill(0);
         finalTicket[1] = Array(9).fill(0);
         finalTicket[2] = Array(9).fill(0);
-        let r = getUniqueRandomNumber(0, 8, 3);
+        var r = getUniqueRandomNumber(0, 8, 3);
         for (i = 0; i < r.length; i++) {
             cols[r[i]] = 1;
         }
@@ -23,12 +23,12 @@ const generateTickets = (tickets) => {
         finalTickets.push(finalTicket);
     }
 }
-const sortNumbersinArray = (a, b) => {
+function sortNumbersinArray (a, b) {
     return a > b ? 1 : b > a ? -1 : 0;
 }
-const getUniqueRandomNumber = (min, max, count) => {
-    let random = [];
-    for (let i = 0; i < count; i++) {
+function getUniqueRandomNumber (min, max, count) {
+    var random = [];
+    for (var i = 0; i < count; i++) {
         flag = true;
         while (flag) {
             r = randomNumber(min, max)
@@ -41,9 +41,11 @@ const getUniqueRandomNumber = (min, max, count) => {
     random.sort(sortNumbersinArray)
     return random;
 }
-const randomNumber = (min, max) => {
+function randomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-export const generateTickets = generateTickets;
+module.exports = {
+    generateTickets: generateTickets
+  }
